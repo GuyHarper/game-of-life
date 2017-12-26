3c30f72fcd51d9a0bbea988794bdc6f7cfe10c32
= <guy.harper@gmail.com>
2017-12-26 18:05:33 +0000

    Adding divs dynamically

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 $(() => {
 
+  const config = {
+    numberOfSquares: 100
+  };
 
+  for(let i = 0; i < config.numberOfSquares; i++) {
+    $('main').append(`<div class="square" id=${i}></div>`);
+  }
 
 });
