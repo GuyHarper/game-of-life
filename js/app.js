18374f8acd62bd7dbf054b7935c296a3ee51a80b
= <guy.harper@gmail.com>
2017-12-26 18:23:48 +0000

    Adding active label

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 $(() => {
 
   const config = {
-    numberOfSquares: 100
+    numberOfSquares: 100,
+    activeSquares: [37,39]
   };
 
   for(let i = 0; i < config.numberOfSquares; i++) {
-    $('main').append(`<div class="square" id=${i}></div>`);
+    let activeSignifier = '';
+    if(config.activeSquares.includes(i)) activeSignifier = ' active';
+    const element = `<div class="square${activeSignifier}" id=${i}></div>`;
+    $('main').append(element);
   }
+
+  const squares = $('div.square');
+
 
 });
