9e757c148096e78b0353821a217dc354133c0d1e
= <guy.harper@gmail.com>
2017-12-26 19:51:43 +0000

    Identifying which squares need checking

--- js/app.js
+++ js/app.js
@@ -5,30 +5,26 @@ $(() => {
     numberOfSquares: 100,
     activeSquares: [28, 37, 39]
   };
-
-  for(let i = 0; i < config.numberOfSquares; i++) {
-    let activeSignifier = '';
-    if(config.activeSquares.includes(i)) activeSignifier = ' active';
-    const element = `<div class="square${activeSignifier}" id=${i}></div>`;
-    $('main').append(element);
-  }
-
-  const squares = $('div.square');
   const neighbourDifferences = [1, config.numberOfColumns, config.numberOfColumns + 1, config.numberOfColumns - 1];
-  console.log(neighbourDifferences);
+
+  function setup() {
+    for(let i = 0; i < config.numberOfSquares; i++) {
+      let activeSignifier = '';
+      if(config.activeSquares.includes(i)) activeSignifier = ' active';
+      const element = `<div class="square${activeSignifier}" id=${i}></div>`;
+      $('main').append(element);
+    }
+  }
 
   function checkNeighbours(squareId) {
     const numberOfActiveNeighbours = config.activeSquares.reduce((accumulator, currentSquareId) => {
       const difference = Math.abs(squareId - currentSquareId);
-      console.log('currentSquareId', currentSquareId);
-      console.log('difference', difference);
       if(neighbourDifferences.includes(difference)) {
         return accumulator + 1;
       } else {
         return accumulator;
       }
     },0);
-    console.log(numberOfActiveNeighbours);
     if(config.activeSquares.includes(squareId)) {
       if(numberOfActiveNeighbours > 3 || numberOfActiveNeighbours < 2) {
         config.activeSquares.splice(config.activeSquares.indexOf(squareId), 1);
@@ -36,7 +32,31 @@ $(() => {
     } else if (numberOfActiveNeighbours === 3) {
       config.activeSquares.push(squareId);
     }
-    console.log(config.activeSquares);
   }
+
+  function identifySurroundingSquares(squareId) {
+    const surroundingSquares = [];
+    neighbourDifferences.forEach((difference) => {
+      surroundingSquares.push(squareId + difference);
+      surroundingSquares.push(squareId - difference);
+    });
+    return surroundingSquares;
+  }
+
+  setup();
+  setInterval(() => {
+    const squaresToCheck = [];
+    config.activeSquares.forEach((square) => {
+      squaresToCheck.push(square);
+      const surroundingSquares = identifySurroundingSquares(square);
+      surroundingSquares.forEach((square) => {
+        squaresToCheck.push(square);
+      });
+    });
+    const uniques = squaresToCheck.filter((element, index) => {
+      return squaresToCheck.indexOf(element) === index;
+    });
+    console.log(uniques);
+  },1000);
 
 });
