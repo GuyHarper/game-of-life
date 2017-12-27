d53f500bd25356b0121eb5816e58c87eeec99812
= <guy.harper@gmail.com>
2017-12-27 13:44:53 +0000

    Adding header with start/stop button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 $(() => {
 
+  const $button = $('button');
   const config = {
     numberOfColumns: 100,
     numberOfSquares: 1000,
-    activeSquares: [24, 27, 33],
+    activeSquares: [124, 224, 324],
     nextRoundActiveSquares: []
   };
   const neighbourDifferences = [1, config.numberOfColumns, config.numberOfColumns + 1, config.numberOfColumns - 1];
+  let running = false;
+  let intervalId = null;
 
   function setup() {
     $('main').css('width', `${config.numberOfColumns * 12}`);
@@ -46,30 +49,46 @@ $(() => {
   }
 
   setup();
-  const intervalId = setInterval(() => {
-    const squaresToCheck = [];
-    config.activeSquares.forEach((square) => {
-      $(`#${square}`).removeClass('alive');
-      squaresToCheck.push(square);
-      const surroundingSquares = identifySurroundingSquares(square);
-      surroundingSquares.forEach((square) => {
+
+  function runLife() {
+    intervalId = setInterval(() => {
+      const squaresToCheck = [];
+      config.activeSquares.forEach((square) => {
+        $(`#${square}`).removeClass('alive');
         squaresToCheck.push(square);
+        const surroundingSquares = identifySurroundingSquares(square);
+        surroundingSquares.forEach((square) => {
+          squaresToCheck.push(square);
+        });
       });
-    });
-    const uniqueSquaresToCheck = squaresToCheck.filter((element, index) => {
-      return squaresToCheck.indexOf(element) === index;
-    });
-    uniqueSquaresToCheck.forEach((square) => {
-      checkNeighbours(square);
-    });
-    config.activeSquares = config.nextRoundActiveSquares;
-    config.activeSquares.forEach((square) => {
-      $(`#${square}`).addClass('alive');
-    });
-    config.nextRoundActiveSquares = [];
-    if(config.activeSquares.length === 0) {
+      const uniqueSquaresToCheck = squaresToCheck.filter((element, index) => {
+        return squaresToCheck.indexOf(element) === index;
+      });
+      uniqueSquaresToCheck.forEach((square) => {
+        checkNeighbours(square);
+      });
+      config.activeSquares = config.nextRoundActiveSquares;
+      config.activeSquares.forEach((square) => {
+        $(`#${square}`).addClass('alive');
+      });
+      config.nextRoundActiveSquares = [];
+      if(config.activeSquares.length === 0) {
+        clearInterval(intervalId);
+        $button.text('Start');
+      }
+    },200);
+  }
+
+  $button.on('click', () => {
+    if (running) {
       clearInterval(intervalId);
+      $button.text('Start');
+      running = false;
+    } else {
+      runLife();
+      $button.text('Stop');
+      running = true;
     }
-  },300);
+  });
 
 });
