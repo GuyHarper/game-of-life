1f2f644d186f0f9188f1f86fd81fa2f5a21cd1a3
= <guy.harper@gmail.com>
2017-12-27 17:21:41 +0000

    Adding title display

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(() => {
 
   const $main = $('main');
   const $startStopButton = $('button.start');
-  const $resetButton = $('button.reset');
+  const $clearButton = $('button.clear');
   const config = {
     squareSize: 10
   };
@@ -12,11 +12,35 @@ $(() => {
   let running = false;
   let intervalId = null;
 
+  function drawTitle() {
+    const gCoordinates = [{x: 1,y: 1},{x: 2,y: 1},{x: 3,y: 1},{x: 1,y: 2},{x: 1,y: 3},{x: 1,y: 4},{x: 3,y: 4},{x: 1,y: 5},{x: 2,y: 5},{x: 3,y: 5}];
+    const aCoordinates = [{x: 1,y: 1},{x: 2,y: 1},{x: 3,y: 1},{x: 1,y: 2},{x: 3,y: 2},{x: 1,y: 3},{x: 2,y: 3},{x: 3,y: 3},{x: 1,y: 4},{x: 3,y: 4},{x: 1,y: 5},{x: 3,y: 5}];
+    const mCoordinates = [{x: 1,y: 1},{x: 3,y: 1},{x: 1,y: 2},{x: 2,y: 2},{x: 3,y: 2},{x: 1,y: 3},{x: 2,y: 3},{x: 3,y: 3},{x: 1,y: 4},{x: 3,y: 4},{x: 1,y: 5},{x: 3,y: 5}];
+    const eCoordinates = [{x: 1,y: 1},{x: 2,y: 1},{x: 3,y: 1},{x: 1,y: 2},{x: 1,y: 3},{x: 2,y: 3},{x: 1,y: 4},{x: 1,y: 5},{x: 2,y: 5},{x: 3,y: 5}];
+    const oCoordinates = [{x: 1,y: 1},{x: 2,y: 1},{x: 3,y: 1},{x: 1,y: 2},{x: 3,y: 2},{x: 1,y: 3},{x: 3,y: 3},{x: 1,y: 4},{x: 3,y: 4},{x: 1,y: 5},{x: 2,y: 5},{x: 3,y: 5}];
+    const fCoordinates = [{x: 1,y: 1},{x: 2,y: 1},{x: 3,y: 1},{x: 1,y: 2},{x: 1,y: 3},{x: 2,y: 3},{x: 1,y: 4},{x: 1,y: 5}];
+    const lCoordinates = [{x: 1,y: 1},{x: 1,y: 2},{x: 1,y: 3},{x: 1,y: 4},{x: 1,y: 5},{x: 2,y: 5},{x: 3,y: 5}];
+    const iCoordinates = [{x: 1,y: 1},{x: 2,y: 1},{x: 3,y: 1},{x: 2,y: 2},{x: 2,y: 3},{x: 2,y: 4},{x: 1,y: 5},{x: 2,y: 5},{x: 3,y: 5}];
+    const titleArray = [[gCoordinates,aCoordinates,mCoordinates,eCoordinates],[oCoordinates,fCoordinates],[lCoordinates,iCoordinates,fCoordinates,eCoordinates]];
+    const centreX = Math.floor(config.numberOfColumns / 2);
+    const centreY = Math.floor(config.numberOfRows / 2);
+    let wordStartingPosition = 0;
+    titleArray.forEach((wordArray, wordIndex) => {
+      if(wordIndex > 0) wordStartingPosition += (titleArray[wordIndex - 1]).length * 4 + 2;
+      wordArray.forEach((letterCoordinates, letterIndex) => {
+        letterCoordinates.forEach((coordinatePair) => {
+          aliveSquares.push(config.numberOfColumns * (centreY - 6 + coordinatePair['y']) + (centreX - 21 + ((coordinatePair['x'] + 4 * letterIndex) + wordStartingPosition)));
+        });
+      });
+    });
+  }
+
   function setup() {
     config.numberOfColumns = Math.floor(window.innerWidth / (config.squareSize + 2)) + 2;
     config.numberOfRows = Math.floor(0.9 * window.innerHeight / (config.squareSize + 2)) + 2;
     config.numberOfSquares = config.numberOfColumns * config.numberOfRows;
     neighbourDifferences = [1, config.numberOfColumns, config.numberOfColumns + 1, config.numberOfColumns - 1];
+    drawTitle();
     $main.css('width', `${config.numberOfColumns * 12}`);
     for(let i = 0; i < config.numberOfSquares; i++) {
       let aliveSignifier = '';
@@ -82,7 +106,7 @@ $(() => {
         running = false;
         $startStopButton.text('Start');
       }
-    },200);
+    },150);
   }
 
   $startStopButton.on('click', () => {
@@ -97,7 +121,7 @@ $(() => {
     }
   });
 
-  $resetButton.on('click', () => {
+  $clearButton.on('click', () => {
     clearInterval(intervalId);
     $startStopButton.text('Start');
     running = false;
@@ -109,6 +133,7 @@ $(() => {
   });
 
   $main.on('click', '.square', (e) => {
+    console.log(e.target.id);
     if (aliveSquares.includes(parseInt(e.target.id))) {
       aliveSquares.splice(aliveSquares.indexOf(parseInt(e.target.id)), 1);
       $(e.target).removeClass('alive');
